f9a54353383570c7fc727b683157b9c5dcacc830
Vaishnavi <vs2753@nyu.edu>
2025-02-17 15:39:14 +0100

    connecting to render

--- seedDatabase/seed.js
+++ seedDatabase/seed.js
@@ -4,7 +4,10 @@ const axios = require('axios');
 dotenv.config({path:'../.env'});
 const {getKey} = require( '../Dal/expressPackages.js');
 const axiosRetry = require("axios-retry").default;
+const CRUD = require( '../Dal/seedDal.js');
+const {sequelize} = require('../Dal/expressPackages.js');
 
+console.log(sequelize);
 
 axiosRetry(axios, {retries: 3, retryDelay: axiosRetry.exponentialDelay});
 
@@ -18,46 +21,52 @@ async function getLocation(name){
 
 async function getSearchData (simpleQuery){
     const key = await getKey();
-    let next = "https://api.artsy.net/api/artworks?page=1";
-    do{
+    let next = "https://api.artsy.net/api/artworks?page=1&total_count=1";
+    console.log(next);
+    try{
+        await sequelize.sync();
+
+    }catch (err){
+        console.log(err);
+    }
     
-        const response = await axios.get(next, {
+
+    const response = await axios.get(next, {
+        headers: {
+            "Accept": "application/json",
+            "X-Xapp-Token": key
+        }
+    });
+    next = response.data._links.next.href;
+    for(const piece of  response.data._embedded.artworks){
+        //save title, date, artists (follow api call, save first name), museum name (collecting_institution)
+        const response2 = await axios.get(piece._links.genes.href, {
             headers: {
                 "Accept": "application/json",
                 "X-Xapp-Token": key
             }
         });
-        next = response.data._links.next.href;
-        for(const piece of  response.data._embedded.artworks){
-            //save title, date, artists (follow api call, save first name), museum name (collecting_institution)
-            const response2 = await axios.get(piece._links.genes.href, {
+        if(piece.collecting_institution &&JSON.stringify(response2.data._embedded.genes).indexOf(simpleQuery) != -1) {
+            const artistRes = await axios.get(piece._links.artists.href, {
                 headers: {
                     "Accept": "application/json",
                     "X-Xapp-Token": key
                 }
             });
-            if(piece.collecting_institution &&JSON.stringify(response2.data._embedded.genes).indexOf(simpleQuery) != -1) {
-                const artistRes = await axios.get(piece._links.artists.href, {
-                    headers: {
-                        "Accept": "application/json",
-                        "X-Xapp-Token": key
-                    }
-                });
-                let artistName = null;
-                if (artistRes.data._embedded.artists[0]){
-                    artistName = artistRes.data._embedded.artists[0].name;
-                }
-
-                    
-                    //create object function call
-                    //put in DB
-                const artworkPL = {title: piece.title, year: piece.date,artist: artistName, musuem_name : piece.collecting_institution.split(', ')[0]};
-                const address = await getLocation(piece.collecting_institution.split(', ')[0]);
-                //if no city for this place, 
-                const cityPL = {city: address.city, state: address.state, country: address.country}
+            let artistName = null;
+            if (artistRes.data._embedded.artists[0]){
+                artistName = artistRes.data._embedded.artists[0].name;
             }
-        } 
-    } while (next)
+
+                
+                //create object function call
+                //put in DB
+            const artworkPL = {title: piece.title, year: piece.date,artist: artistName, musuem_name : piece.collecting_institution.split(', ')[0]};
+            const address = await getLocation(piece.collecting_institution.split(', ')[0]);
+            //if no city for this place, 
+            const cityPL = {city: address.city, state: address.state, country: address.country}
+        }
+    } 
     // initialize sequelize instance and add the payloads into the database. 
         
     };
